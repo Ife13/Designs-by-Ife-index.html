561714e1362170cd339d376b134a2047f85e33ac
Ife Njock-Barnes <ife1998@hotmail.com>
2023-07-20 23:16:10 +0100

    further add ons

--- js/modal.js
+++ js/modal.js
@@ -2,15 +2,19 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg1");
+var img = document.getElementsByClassName("myImages");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function () {
+
+var showModal = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 };
-
+// Go through all of the images with our custom class
+for (var i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", showModal);
+}
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
